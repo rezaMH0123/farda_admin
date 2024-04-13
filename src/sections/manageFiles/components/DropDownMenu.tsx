30c080727480cef7f4858de76b951d20a9de1130
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-04-13 11:20:33 +0330

    create dropdown menu

---
+++ src/sections/manageFiles/components/DropDownMenu.tsx
@@ -0,0 +1,40 @@
+import { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import More from "@/assets/img/tools/more.svg";
+
+export default function DropDownMenu() {
+  return (
+    <Menu as="div" className="relative inline-block text-center">
+      <Menu.Button>
+        <img src={More} alt="more" className="cursor-pointer mt-2" />
+      </Menu.Button>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="origin-top-right absolute left-[-10px] mt-1 w-56 rounded-md shadow-lg bg-PrimaryBlack-300 ring-1 ring-black ring-opacity-5 divide-y divide-gray-300 focus:outline-none">
+          <div className="py-1">
+            <Menu.Item>
+              <p className="cursor-pointer">ویرایش</p>
+            </Menu.Item>
+          </div>
+          <div className="py-1">
+            <Menu.Item>
+              <p className="cursor-pointer">اشتراک‌گذاری</p>
+            </Menu.Item>
+          </div>
+          <div className="py-1">
+            <Menu.Item>
+              <p className="cursor-pointer">حذف</p>
+            </Menu.Item>
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  );
+}
