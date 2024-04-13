1a451bd9316ef7c22f34f75c2e340467be7ee8db
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-04-13 14:35:51 +0330

    create drop down menu items

---
+++ src/constants/items/dropDownMenuItems.ts
@@ -0,0 +1,26 @@
+export const FileMenuItems = [
+  {
+    name: "ویرایش",
+  },
+  {
+    name: "تغییر اسم",
+  },
+  {
+    name: "اشتراک‌گذاری",
+  },
+  {
+    name: "حذف",
+  },
+];
+
+export const PhotoMenuItems = [
+  {
+    name: "ویرایش",
+  },
+  {
+    name: "اشتراک‌گذاری",
+  },
+  {
+    name: "حذف",
+  },
+];
--- src/sections/manageFiles/components/DropDownMenu.tsx
+++ src/sections/manageFiles/components/DropDownMenu.tsx
@@ -2,7 +2,15 @@ import { Fragment } from "react";
 import { Menu, Transition } from "@headlessui/react";
 import More from "@/assets/img/tools/more.svg";
 
-export default function DropDownMenu() {
+type MenuItemsT = {
+  name: string;
+};
+
+export type DropDownMenuProps = {
+  menuItemsT: MenuItemsT[];
+};
+
+export default function DropDownMenu({ menuItemsT }: DropDownMenuProps) {
   return (
     <Menu as="div" className="relative inline-block text-center">
       <Menu.Button>
@@ -18,21 +26,13 @@ export default function DropDownMenu() {
         leaveTo="transform opacity-0 scale-95"
       >
         <Menu.Items className="origin-top-right absolute left-[-10px] mt-1 w-56 rounded-md shadow-lg bg-PrimaryBlack-300 ring-1 ring-black ring-opacity-5 divide-y divide-gray-300 focus:outline-none">
-          <div className="py-1">
-            <Menu.Item>
-              <p className="cursor-pointer">ویرایش</p>
-            </Menu.Item>
-          </div>
-          <div className="py-1">
-            <Menu.Item>
-              <p className="cursor-pointer">اشتراک‌گذاری</p>
-            </Menu.Item>
-          </div>
-          <div className="py-1">
-            <Menu.Item>
-              <p className="cursor-pointer">حذف</p>
-            </Menu.Item>
-          </div>
+          {menuItemsT.map((item, index) => (
+            <div key={index} className="py-1">
+              <Menu.Item>
+                <p className="cursor-pointer">{item.name}</p>
+              </Menu.Item>
+            </div>
+          ))}
         </Menu.Items>
       </Transition>
     </Menu>
