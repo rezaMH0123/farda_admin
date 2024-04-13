458567bc057511aa5d1685f5570bbacc04ddb450
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-04-13 09:12:55 +0330

    remove unnecessary arrays from manageFiles index

--- src/sections/manageFiles/index.tsx
+++ src/sections/manageFiles/index.tsx
@@ -6,8 +6,6 @@ import ManageFileBodySection from "./components/FileBody";
 
 export default function ManageFile() {
   const [openModal, setOpenModal] = useState<boolean>(false);
-  const [photos, setPhotos] = useState<File[]>([]);
-  const [files, setFiles] = useState<File[]>([]);
 
   return (
     <div
@@ -15,17 +13,11 @@ export default function ManageFile() {
       className="border border-[#E1E1E1] w-[80%] h-[85%] rounded-xl "
     >
       <ManageFileHeaderSection setOpenModal={setOpenModal} />
-      <ManageFileBodySection photos={photos} files={files} />
+      <ManageFileBodySection />
       {openModal && (
         <Modal width={35} height={55} setOpenModal={setOpenModal}>
           <div className="w-full h-full">
-            <ManageFileModalBody
-              setOpenModal={setOpenModal}
-              photos={photos}
-              setPhotos={setPhotos}
-              files={files}
-              setFiles={setFiles}
-            />
+            <ManageFileModalBody setOpenModal={setOpenModal} />
           </div>
         </Modal>
       )}
