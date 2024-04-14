5834f8937eb2de211d382ac145341a453f98800c
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-04-14 14:55:46 +0330

    create cardFile and photoCard

---
+++ src/sections/manageFiles/components/Cards/CardPhoto.tsx
@@ -0,0 +1,31 @@
+import { FilesI } from "@/types/models/Files.type";
+import Square from "@/assets/img/tools/square.svg";
+import { PhotoMenuItems } from "@/constants/items/dropDownMenuItems";
+import DropDownMenu from "../DropDownMenu";
+
+interface CardPhotoProps {
+  item: FilesI;
+}
+
+export default function CardPhoto({ item }: CardPhotoProps) {
+  return (
+    <div
+      key={item.id}
+      className="h-[200px] w-[100%] rounded-[20px] bg-[#DEE8FF] custom-shadow"
+    >
+      <div className="w-[91%] h-[20%] m-auto flex items-center justify-between">
+        <img src={Square} alt="square" className="cursor-pointer" />
+        <DropDownMenu items={item} menuItemsT={PhotoMenuItems} />
+      </div>
+      <div
+        className="w-[95%] h-[123px] rounded m-auto bg-cover bg-no-repeat"
+        style={{ backgroundImage: `url(${item.fileUrl})` }}
+      ></div>
+      <div className="w-full h-[20%] flex items-center justify-center">
+        <p className="font-normal text-PrimaryBlack-500">
+          سه‌شنبه 21 فروردین 1403
+        </p>
+      </div>
+    </div>
+  );
+}
