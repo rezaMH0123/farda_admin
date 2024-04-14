a0118fc9f4dd39ed4a412f07473bc66d09bec8ef
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-04-14 09:59:30 +0330

    edit button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -7,7 +7,12 @@ type ButtonPropsT = {
   disable?: boolean;
   onClick?: () => void;
   icon?: ReactNode;
+<<<<<<< HEAD
   model?: "fill_blue" | "fill_red" | "outline_gray" | "outline_red";
+=======
+  background?: string;
+  textColor?: string;
+>>>>>>> d470c0c (edit button component)
 };
 
 export default function Button({
