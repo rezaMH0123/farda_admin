e5244545a850b3679c31acd2c6f62767779148a3
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-04-14 14:55:46 +0330

    create cardFile and photoCard

---
+++ src/sections/manageFiles/components/Cards/CardFile.tsx
@@ -0,0 +1,63 @@
+import { FilesI } from "@/types/models/Files.type";
+import DropDownMenu from "../DropDownMenu";
+import { FileMenuItems } from "@/constants/items/dropDownMenuItems";
+import Pdf from "@/assets/img/tools/pdf.svg";
+import Word from "@/assets/img/tools/word.svg";
+import Exel from "@/assets/img/tools/exel.svg";
+import PdfFile from "@/assets/img/tools/file.svg";
+import WordFile from "@/assets/img/tools/wordFile.svg";
+import ExelFile from "@/assets/img/tools/excelFile.svg";
+
+interface CardFileProps {
+  item: FilesI;
+}
+
+export default function CardFile({ item }: CardFileProps) {
+  const fileType: string = item.extention;
+  return (
+    <div
+      key={item.id}
+      className="h-[200px] w-[100%] rounded-[20px] bg-[#DEE8FF] custom-shadow"
+    >
+      <div className="w-[91%] h-[20%] m-auto flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <img
+            src={
+              fileType === ".pdf"
+                ? Pdf
+                : fileType === ".docx"
+                ? Word
+                : fileType === ".xlsx" || fileType === ".xls"
+                ? Exel
+                : ""
+            }
+            alt="fileType"
+          />
+          <p className="font-normal text-xs text-PrimaryBlack-300">
+            {item.filename}
+          </p>
+        </div>
+        <DropDownMenu items={item} menuItemsT={FileMenuItems} />
+      </div>
+      <div className="w-[95%] h-[123px] flex justify-center items-center rounded m-auto">
+        <img
+          src={
+            fileType === ".pdf"
+              ? PdfFile
+              : fileType === ".docx"
+              ? WordFile
+              : fileType === ".xlsx" || fileType === ".xls"
+              ? ExelFile
+              : ""
+          }
+          alt="file"
+        />
+      </div>
+      <div className="w-full h-[20%] flex items-center justify-center">
+        <p className="font-normal text-PrimaryBlack-500">
+          سه‌شنبه 21 فروردین 1403
+        </p>
+      </div>
+    </div>
+  );
+}
