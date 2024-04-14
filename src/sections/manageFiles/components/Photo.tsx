0f7be71b3923c57cb66572bbb72d1cfa390bb7d7
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-04-14 14:55:04 +0330

    fixed useGlobalState context for file and photo

--- src/sections/manageFiles/components/Photo.tsx
+++ src/sections/manageFiles/components/Photo.tsx
@@ -1,35 +1,51 @@
 import { FilesI } from "@/types/models/Files.type";
-import Square from "@/assets/img/tools/square.svg";
-import DropDownMenu from "./DropDownMenu";
-import { PhotoMenuItems } from "@/constants/items/dropDownMenuItems";
+import { useModal } from "@/context/modalContext";
+import Modal from "@/components/Modal";
+import IconDelete from "@/components/Icons/DeleteIcon";
+import Button from "@/components/Button";
+import { useGlobalState } from "@/context/globalStateContext";
+import CardPhoto from "./Cards/CardPhoto";
 
 export default function Photos({ photos }: { photos?: FilesI[] }) {
+  const { isDeleteModalOpen, closeDeleteModal } = useModal();
+  const { itemFile } = useGlobalState();
+
+  console.log(itemFile && itemFile);
   return (
     <>
       {photos?.length === 0 ? (
         <>there is no photos</>
       ) : (
-        photos?.map((item) => (
-          <div
-            key={item.id}
-            className="h-[200px] w-[100%] rounded-[20px] bg-[#DEE8FF] custom-shadow"
-          >
-            <div className="w-[91%] h-[20%] m-auto flex items-center justify-between">
-              <img src={Square} alt="square" className="cursor-pointer" />
-              <DropDownMenu menuItemsT={PhotoMenuItems} />
-            </div>
-            <div
-              className="w-[95%] h-[123px] rounded m-auto bg-cover bg-no-repeat"
-              style={{ backgroundImage: `url(${item.fileUrl})` }}
-            ></div>
-            <div className="w-full h-[20%] flex items-center justify-center">
-              <p className="font-normal text-PrimaryBlack-500">
-                سه‌شنبه 21 فروردین 1403
-              </p>
-            </div>
-          </div>
-        ))
+        photos?.map((item) => <CardPhoto key={item.id} item={item} />)
       )}
+      <>
+        {isDeleteModalOpen && (
+          <Modal width={25} height={38}>
+            <div className="w-full h-full">
+              <div className="w-full h-full flex items-center flex-col">
+                <IconDelete className="mt-[82px]" />
+                <p className="mt-4 text-PrimaryBlack-800 text-xs">
+                  {itemFile?.filename}
+                  {itemFile?.extention}
+                </p>
+                <div className="w-[70%] h-[44px] m-auto flex gap-5 mt-12">
+                  <Button
+                    className="text-sm w-[50%]"
+                    model="outline_gray"
+                    title="منصرف شدم"
+                    onClick={closeDeleteModal}
+                  />
+                  <Button
+                    className="text-sm w-[50%]"
+                    model="fill_red"
+                    title="حذف شود"
+                  />
+                </div>
+              </div>
+            </div>
+          </Modal>
+        )}
+      </>
     </>
   );
 }
