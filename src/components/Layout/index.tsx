491013cbcfea70251eddb87f3dd1af49984ae726
rezaMH0123 <80916630+rezaMH0123@users.noreply.github.com>
2024-04-15 17:02:12 +0330

    react queru used

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,21 +3,25 @@ import Header from "./Header";
 import SideBar from "./SideBar";
 import { Outlet } from "react-router-dom";
 import { GlobalStateProvider } from "@/context/globalStateContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function Layout() {
+  const queryClient = new QueryClient();
   return (
     <div className="flex justify-center px-12 py-4 h-full w-screen">
-      <ModalProvider>
-        <GlobalStateProvider>
-          <div className="w-[1440px] h-full">
-            <Header />
-            <div className="flex justify-between gap-x-3  h-full mt-6">
-              <Outlet />
-              <SideBar />
+      <QueryClientProvider client={queryClient}>
+        <ModalProvider>
+          <GlobalStateProvider>
+            <div className="w-[1440px] h-full">
+              <Header />
+              <div className="flex justify-between gap-x-3  h-full mt-6">
+                <Outlet />
+                <SideBar />
+              </div>
             </div>
-          </div>
-        </GlobalStateProvider>
-      </ModalProvider>
+          </GlobalStateProvider>
+        </ModalProvider>
+      </QueryClientProvider>
     </div>
   );
 }
--- src/sections/content/components/ContentChart.tsx
+++ src/sections/content/components/ContentChart.tsx
@@ -1,62 +1,49 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Pagination from "@/components/Pagination";
 import Edit from "@/components/Icons/Edit";
 import RecycleBin from "@/components/Icons/RecycleBin";
+import http from "@/core/services/httpServices";
+import TailSpinner from "@/components/Loading/TailSpinner";
+import { Advertisement } from "@/types/models/Content.type";
+import { HttpResponseList } from "@/types/httpResponse";
+import Cookies from "js-cookie";
+import { useQuery } from "@tanstack/react-query";
+
+const getContent = async (currentPage: number) => {
+  try {
+    const res = await http.get<HttpResponseList<Advertisement>>(
+      "Panel/Content",
+      {
+        params: {
+          Size: 6,
+          Page: currentPage,
+          Sort: "createdOn",
+        },
+        headers: {
+          Authorization: `Bearer ${Cookies.get("access_token")}`, // Add the token to the Authorization header
+        },
+      }
+    );
+    return res.data; // Return the data after successfully fetching
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to fetch content"); // Throw error to be caught by React Query
+  }
+};
 
 export default function ContentChart() {
   const [allPage] = useState<number>(8);
 
   const [currentPage, setCurrentPage] = useState<number>(1);
-  const [data] = useState([
-    {
-      id: 1,
-      title: "آگهی دعوت به مجمع عمومی عادی سالیانه...",
-      status: "منتشر شده",
-      shareable: "می شود",
-      commentable: "نمی شود",
-      checked: false,
-    },
-    {
-      id: 2,
-      title: "آگهی دعوت به مجمع عمومی عادی سالیانه...",
-      status: "منتشر شده",
-      shareable: "می شود",
-      commentable: "نمی شود",
-      checked: false,
-    },
-    {
-      id: 3,
-      title: "آگهی دعوت به مجمع عمومی عادی سالیانه...",
-      status: "منتشر شده",
-      shareable: "می شود",
-      commentable: "نمی شود",
-      checked: false,
-    },
-    {
-      id: 4,
-      title: "آگهی دعوت به مجمع عمومی عادی سالیانه...",
-      status: "منتشر شده",
-      shareable: "می شود",
-      commentable: "نمی شود",
-      checked: false,
-    },
-    {
-      id: 5,
-      title: "آگهی دعوت به مجمع عمومی عادی سالیانه...",
-      status: "منتشر شده",
-      shareable: "می شود",
-      commentable: "نمی شود",
-      checked: false,
-    },
-    {
-      id: 6,
-      title: "آگهی دعوت به مجمع عمومی عادی سالیانه...",
-      status: "منتشر شده",
-      shareable: "می شود",
-      commentable: "نمی شود",
-      checked: false,
-    },
-  ]);
+
+  const { data, isError, isLoading } = useQuery<
+    HttpResponseList<Advertisement>
+  >({
+    queryKey: ["content", currentPage],
+    queryFn: () => getContent(currentPage),
+    retry: false,
+    refetchOnWindowFocus: true,
+  });
 
   const nextPageClick = () => {
     setCurrentPage((prev) => prev + 1);
@@ -64,6 +51,8 @@ export default function ContentChart() {
   const prevPageClick = () => {
     setCurrentPage((prev) => prev - 1);
   };
+
+  console.log(data?.data.result);
 
   return (
     <div className="chartContetnt px-6 h-full w-full">
@@ -86,37 +75,45 @@ export default function ContentChart() {
       </div>
 
       <div className="haedBody order-b border-t border-PrimaryBlack-300 h-[60%] w-full ">
-        {data.map((item) => (
-          <div
-            key={item.id}
-            className="flex items-center h-[14%] text-PrimaryBlack-400  mt-2 "
-          >
-            <div className="flex items-center   w-[40%] h-[40px]">
-              {item.title}
-            </div>
-            <div className="flex items-center justify-center  w-[15%] h-[40px]">
-              {item.status}
-            </div>
-            <div className="flex items-center justify-center  w-[15%] h-[40px]">
-              {item.shareable}
-            </div>
-            <div className="flex items-center justify-center  w-[15%] h-[40px]">
-              {item.commentable}
-            </div>
-            <div className="flex items-center justify-center gap-x-4  w-[15%] h-[40px]">
-              <Edit
-                onClick={() => console.log(`Edit:${item.id}`)}
-                fill="#41CD92"
-                className="cursor-pointer"
-              />
-              <RecycleBin
-                onClick={() => console.log(`RecycleBin:${item.id}`)}
-                fill="#FF8A8A"
-                className="cursor-pointer"
-              />
-            </div>
+        {isLoading ? (
+          <div className="w-full h-full flex justify-center items-center">
+            <TailSpinner />
           </div>
-        ))}
+        ) : (
+          <>
+            {/* {data.map((item) => (
+              <div
+                key={item.id}
+                className="flex items-center h-[14%] text-PrimaryBlack-400  mt-2 "
+              >
+                <div className="flex items-center   w-[40%] h-[40px]">
+                  {item.title}
+                </div>
+                <div className="flex items-center justify-center  w-[15%] h-[40px]">
+                  {item.status}
+                </div>
+                <div className="flex items-center justify-center  w-[15%] h-[40px]">
+                  {item.shareable}
+                </div>
+                <div className="flex items-center justify-center  w-[15%] h-[40px]">
+                  {item.commentable}
+                </div>
+                <div className="flex items-center justify-center gap-x-4  w-[15%] h-[40px]">
+                  <Edit
+                    onClick={() => console.log(`Edit:${item.id}`)}
+                    fill="#41CD92"
+                    className="cursor-pointer"
+                  />
+                  <RecycleBin
+                    onClick={() => console.log(`RecycleBin:${item.id}`)}
+                    fill="#FF8A8A"
+                    className="cursor-pointer"
+                  />
+                </div>
+              </div>
+            ))} */}
+          </>
+        )}
       </div>
 
       <div className="flex gap-x-5 justify-end items-center  h-[15%]">
