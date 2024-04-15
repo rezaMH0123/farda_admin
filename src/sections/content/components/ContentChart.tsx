8d707b1f752e25e025a8be7e8a1ae757534a9e01
rezaMH0123 <80916630+rezaMH0123@users.noreply.github.com>
2024-04-15 17:02:12 +0330

    react queru used

--- src/sections/content/components/ContentChart.tsx
+++ src/sections/content/components/ContentChart.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import Pagination from "@/components/Pagination";
 import Edit from "@/components/Icons/Edit";
 import RecycleBin from "@/components/Icons/RecycleBin";
@@ -36,18 +36,14 @@ export default function ContentChart() {
 
   const [currentPage, setCurrentPage] = useState<number>(1);
 
-  const { data, isLoading } = useQuery<HttpResponseList<Advertisement>>({
+  const { data, isError, isLoading } = useQuery<
+    HttpResponseList<Advertisement>
+  >({
     queryKey: ["content", currentPage],
     queryFn: () => getContent(currentPage),
     retry: false,
     refetchOnWindowFocus: true,
   });
-
-  useEffect(() => {
-    if (data) {
-      setAllPage(Math.ceil(Number(data?.data.totalRowCount) / 6));
-    }
-  }, [data]);
 
   const nextPageClick = () => {
     setCurrentPage((prev) => prev + 1);
@@ -55,6 +51,8 @@ export default function ContentChart() {
   const prevPageClick = () => {
     setCurrentPage((prev) => prev - 1);
   };
+
+  console.log(data?.data.result);
 
   return (
     <div className="chartContetnt px-6 h-full w-full">
