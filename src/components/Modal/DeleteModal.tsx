3f5640f91c5230804c455a832aab2e35682fa20a
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-04-17 15:03:41 +0330

    create delete modal component

---
+++ src/components/Modal/DeleteModal.tsx
@@ -0,0 +1,43 @@
+import IconDelete from "@/components/Icons/DeleteIcon";
+import Button from "@/components/Button";
+import { useModal } from "@/context/modalContext";
+import Loading from "../Loading";
+
+type Props = {
+  title: string | undefined;
+  onClick: () => Promise<void>;
+  loading: boolean;
+};
+
+export default function DeleteModal({ title, onClick, loading }: Props) {
+  const { closeDeleteModal } = useModal();
+
+  return (
+    <div className="w-full h-full">
+      <div className="w-full h-full flex items-center flex-col">
+        <IconDelete className="mt-[82px]" />
+        <p className="mt-4 text-PrimaryBlack-800 text-xs">{title}</p>
+        <div className="w-[70%] h-[44px] m-auto flex gap-5 mt-12">
+          <Button
+            className="text-sm w-[50%]"
+            model="outline_gray"
+            title="منصرف شدم"
+            onClick={closeDeleteModal}
+          />
+          <Button
+            className="text-sm w-[50%]"
+            model="fill_red"
+            title={
+              loading ? (
+                <Loading className={"bg-PrimaryBlack-200"} />
+              ) : (
+                "حذف شود"
+              )
+            }
+            onClick={onClick}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
