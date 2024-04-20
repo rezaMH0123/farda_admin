9a31aa9dd624abe2cf55fd4967270ede90ea1e07
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-04-20 14:13:46 +0330

    add link for files and photos

--- src/sections/manageFiles/components/Cards/CardFile.tsx
+++ src/sections/manageFiles/components/Cards/CardFile.tsx
@@ -7,6 +7,7 @@ import Exel from "@/assets/img/tools/exel.svg";
 import PdfFile from "@/assets/img/tools/file.svg";
 import WordFile from "@/assets/img/tools/wordFile.svg";
 import ExelFile from "@/assets/img/tools/excelFile.svg";
+import { Link } from "react-router-dom";
 
 interface CardFileProps {
   item: FilesI;
@@ -39,20 +40,22 @@ export default function CardFile({ item }: CardFileProps) {
         </div>
         <DropDownMenu items={item} menuItemsT={FileMenuItems} />
       </div>
-      <div className="w-[95%] h-[123px] flex justify-center items-center rounded m-auto">
-        <img
-          src={
-            fileType === ".pdf"
-              ? PdfFile
-              : fileType === ".docx"
-              ? WordFile
-              : fileType === ".xlsx" || fileType === ".xls"
-              ? ExelFile
-              : ""
-          }
-          alt="file"
-        />
-      </div>
+      <Link to={item.fileUrl} target="_blank">
+        <div className="w-[95%] h-[123px] flex justify-center items-center rounded m-auto">
+          <img
+            src={
+              fileType === ".pdf"
+                ? PdfFile
+                : fileType === ".docx"
+                ? WordFile
+                : fileType === ".xlsx" || fileType === ".xls"
+                ? ExelFile
+                : ""
+            }
+            alt="file"
+          />
+        </div>
+      </Link>
       <div className="w-full h-[20%] flex items-center justify-center">
         <p className="font-normal text-PrimaryBlack-500">
           سه‌شنبه 21 فروردین 1403
--- src/sections/manageFiles/components/Cards/CardPhoto.tsx
+++ src/sections/manageFiles/components/Cards/CardPhoto.tsx
@@ -2,6 +2,7 @@ import { FilesI } from "@/types/models/Files.type";
 import Square from "@/assets/img/tools/square.svg";
 import { PhotoMenuItems } from "@/constants/items/dropDownMenuItems";
 import DropDownMenu from "../DropDownMenu";
+import { Link } from "react-router-dom";
 
 interface CardPhotoProps {
   item: FilesI;
@@ -17,10 +18,12 @@ export default function CardPhoto({ item }: CardPhotoProps) {
         <img src={Square} alt="square" className="cursor-pointer" />
         <DropDownMenu items={item} menuItemsT={PhotoMenuItems} />
       </div>
-      <div
-        className="w-[95%] h-[123px] rounded m-auto bg-cover bg-no-repeat"
-        style={{ backgroundImage: `url(${item.fileUrl})` }}
-      ></div>
+      <Link to={item.fileUrl} target="_blank">
+        <div
+          className="w-[95%] h-[123px] rounded m-auto bg-cover bg-no-repeat"
+          style={{ backgroundImage: `url(${item.fileUrl})` }}
+        />
+      </Link>
       <div className="w-full h-[20%] flex items-center justify-center">
         <p className="font-normal text-PrimaryBlack-500">
           سه‌شنبه 21 فروردین 1403
