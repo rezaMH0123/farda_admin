dcfbd719cb6645305ef6d1c86c8d14d7f8ad3799
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-04-22 11:34:55 +0330

    fixed signin with react query and controller

--- src/types/signin.ts
+++ src/types/signin.ts
@@ -2,3 +2,11 @@ export type SignInInputsT = {
   userName: string;
   password: string;
 };
+
+export interface SiginData {
+  access_token: string;
+  expire_refresh_token: string;
+  expires_in: string;
+  refresh_token: string;
+  token_type: string;
+}
