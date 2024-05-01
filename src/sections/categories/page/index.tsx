1bb0a3c5ef9008d712167102ea958d88617a0d59
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-05-01 13:59:59 +0330

    edit types and keyNames

--- src/sections/categories/page/index.tsx
+++ src/sections/categories/page/index.tsx
@@ -3,7 +3,7 @@ import Modal from "@/components/Modal";
 import TableWithApi from "@/components/TableWithApi";
 import SHARED_STRINGS from "@/constants/strings/shared.string";
 import { CategorieController } from "@/controllers/categorie.contoroller";
-import { CategorieItem } from "@/types/models/Categories.type";
+import { CategorieItem, CategoryMain } from "@/types/models/Categories.type";
 import StringsE from "@/types/strings";
 import { useState } from "react";
 import { useLocation } from "react-router-dom";
@@ -15,23 +15,28 @@ const chartTitles = ["عنوان", "پین بودن", "عملیات"];
 export default function CategoryPage() {
   const [modal, setModal] = useState<boolean>(false);
   const location = useLocation();
+
   const HandleCloseModal = () => {
     setModal(false);
   };
+
   const passedData = location.state?.data;
+
   return (
     <div className="h-full">
-      <PagesHeaderSection setModal={setModal} title={passedData.data} />
-      <TableWithApi<CategorieItem>
-        controller={() => CategorieController.getChilds(passedData.id, 1000, 1)}
+      <PagesHeaderSection setModal={setModal} title={passedData.title} />
+      <TableWithApi<CategoryMain>
+        controller={(currentPage) =>
+          CategorieController.getChilds(passedData.id, currentPage)
+        }
         title={chartTitles}
-        keyNme="categories"
+        keyNme="category"
       >
         {(row) => (
           <RowCategory
             modal={modal}
             setModal={setModal}
-            keyName="categories"
+            keyName="category"
             {...row}
           />
         )}
