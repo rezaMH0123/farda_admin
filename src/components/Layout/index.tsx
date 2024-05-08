32062faed2c6a4b23c6b3092f745bad15c988734
rezaMH0123 <80916630+rezaMH0123@users.noreply.github.com>
2024-05-08 10:48:52 +0330

    add provetRoute

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,24 +4,27 @@ import SideBar from "./SideBar";
 import { Outlet } from "react-router-dom";
 import { GlobalStateProvider } from "@/context/globalStateContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import PrivateRoutes from "@/core/routes/privateRoute";
 
 const queryClient = new QueryClient();
 export default function Layout() {
   return (
-    <div className="flex justify-center px-12 py-4 h-full w-screen">
-      <QueryClientProvider client={queryClient}>
-        <ModalProvider>
-          <GlobalStateProvider>
-            <div className="w-[1440px] h-full">
-              <Header />
-              <div className="flex justify-between gap-x-3 h-full mt-6">
-                <Outlet />
-                <SideBar />
+    <PrivateRoutes>
+      <div className="flex justify-center px-12 py-4 h-full w-screen">
+        <QueryClientProvider client={queryClient}>
+          <ModalProvider>
+            <GlobalStateProvider>
+              <div className="w-[1440px] h-full">
+                <Header />
+                <div className="flex justify-between gap-x-3 h-full mt-6">
+                  <Outlet />
+                  <SideBar />
+                </div>
               </div>
-            </div>
-          </GlobalStateProvider>
-        </ModalProvider>
-      </QueryClientProvider>
-    </div>
+            </GlobalStateProvider>
+          </ModalProvider>
+        </QueryClientProvider>
+      </div>
+    </PrivateRoutes>
   );
 }
